d3a498e8bcc90c5f4f22ee41b3792e0366504de4
Denis <den.denis.orlov@bk.ru>
2022-02-08 13:29:09 +0300

    hamburger

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,12 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+/* const menuIcon = document.querySelector('.menu__icon');
+if (menuIcon){
+    const navManu = document.querySelector('.nav__manu');
+    menuIcon.addEventListener("click", function (e)){
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        navManu.classList.toggle('_active');
+    }
+}*/
